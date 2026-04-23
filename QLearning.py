6cb7d9ec244a2b08c8a1554383516db47e8f3f48
XavZol <alansolis707@gmail.com>
2026-04-23 16:05:31 -0600

    Segundo Algoritmo

--- QLearning.py
+++ QLearning.py
@@ -1,16 +1,16 @@
-import numpy as np 
+# Q(s,a) = Q(s, a) + + — Q(8, a))
+
+import numpy as np
 import random
+import matplotlib.pyplot as plt
 
-# Q-Learning en Machine Learning
-
-dimensiones = (5, 5)
-estado_incial = (0, 0)
-estado_objetivo = (4, 4)
-obstaculos = [(1, 1), (1, 3), (2, 3), (3, 0)]
-acciones = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
+dimensiones = (4, 4)
+estado_inicial = (0, 0)
+estado_objetivo = (3, 3)
+acciones = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+acciones_simbolos = ['↑', '↓', '←', '→']
 num_estados = dimensiones[0] * dimensiones[1]
-print(num_estados)
+print(num_estados)  
 
 num_acciones = len(acciones)
 print(num_acciones)
@@ -18,53 +18,56 @@ print(num_acciones)
 Q = np.zeros((num_estados, num_acciones))
 print(Q)
 
-def estado_a_indice(estado):
-    return estado[0] * dimensiones[1] + estado[1]
-ejemplo = estado_a_indice((0, 0))
-print(ejemplo)
-
 alpha = 0.1
-gamma = 0.99 
-epsilon = 0.2 # Nivel de Porcentaje 
-episodios = 100
+gamma = 0.99
+epsilon = 0.2
+episodios = 1000
+
+# Parámetros Sarsa
+
+def estado_indice(estado):
+    return estado[0] * dimensiones[1] + estado[1]
+
+print(estado_indice((3, 0)))
 
 def elegir_accion(estado):
     if random.uniform(0, 1) < epsilon:
-        return random.choice(range(num_acciones))
+        return random.randint(0, num_acciones - 1)
     else: 
-        return np.argmax(Q[estado_a_indice(estado)])
-    
+        return np.argmax(Q[estado_indice(estado)])
+
 def aplicar_accion(estado, accion_idx):
     accion = acciones[accion_idx]
-    nuevo_estado = tuple(np.add(estado,accion) % dimensiones)
+    nuevo_estado = tuple(np.add(estado, accion) % np.array(dimensiones))
 
-    if nuevo_estado in obstaculos or nuevo_estado == estado:
-        return estado, -100, False
     if nuevo_estado == estado_objetivo:
-        return nuevo_estado, 100, True
-    return nuevo_estado, -1, False
+        recompensa = 1 
+    else: 
+        recompensa = -1
+    
+    return nuevo_estado, recompensa, nuevo_estado == estado_objetivo
 
-for episodios in range(episodios):
-    estado = estado_incial
+for epsilon in range(episodios):
+    estado = estado_inicial
+    accion_idx = elegir_accion(estado)
     terminado = False
 
     while not terminado:
-        idx_estado = estado_a_indice(estado)
-        accion_idx = elegir_accion(estado)
-        nuevo_estado, recompensa, terminado = aplicar_accion(estado, accion_idx)
-        idx_nuevo_estado = estado_a_indice(nuevo_estado)
+        nuevo_estado , recompensa , terminado = aplicar_accion(estado, accion_idx)
+        nueva_accion_idx = elegir_accion(nuevo_estado)
 
-        Q[idx_estado, accion_idx] = Q[idx_estado, accion_idx] + alpha + (recompensa + gamma * np.max(Q[idx_nuevo_estado]) - Q[idx_estado, accion_idx])
-        estado = nuevo_estado
+        indice = estado_indice(estado)
+        Q[indice, accion_idx] += alpha * (recompensa + gamma * Q[estado_indice(nuevo_estado), nueva_accion_idx] - Q[indice, accion_idx])
+        # Lógica del algoritmo
+        estado, accion_idx = nuevo_estado, nueva_accion_idx
 
-politica = np.zeros(dimensiones, dtype=int)
-print(politica)
-
+politica_simbolos = np.empty(dimensiones, dtype='<U2')
 for i in range(dimensiones[0]):
     for j in range(dimensiones[1]):
         estado = (i, j)
-        idx_estado = estado_a_indice(estado)
-        mejor_accion = np.argmax(Q[idx_estado])
-        politica[i, j] = mejor_accion
-print("Política aprendida (0: arriba, 1: abajo, 2: izquierda, 3: derecha)")
-print(politica)
+        mejor_accion = np.argmax(Q[estado_indice(estado)])
+        politica_simbolos[i, j] = acciones_simbolos[mejor_accion]
+        
+print(politica_simbolos)
+
+
